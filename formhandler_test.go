acf3492a7b760d358287ee0732be8aa15dc0c16a
Nathan Marley <nathan.marley@gmail.com>
2018-08-27 13:39:55 -0600

    add test for exercise

--- formhandler_test.go
+++ formhandler_test.go
@@ -9,6 +9,20 @@ import (
 	"testing"
 )
 
+func FormHandler(res http.ResponseWriter, req *http.Request) {
+  err := req.ParseForm()
+  if err != nil {
+    res.WriteHeader(500)
+    return
+  }
+
+  name := req.Form.Get("name")
+  if name == "" {
+    name = "World"
+  }
+  fmt.Fprintf(res, "Posted Hello, %s!", name)
+}
+
 func Test_U_FormHandler(t *testing.T) {
 	// The url.Values type allows us to assemble a "form" that we can send as part
 	// of the request.
@@ -33,16 +47,26 @@ func Test_U_FormHandler(t *testing.T) {
 	}
 }
 
-func FormHandler(res http.ResponseWriter, req *http.Request) {
-  err := req.ParseForm()
-  if err != nil {
-    res.WriteHeader(500)
-    return
-  }
+func Test_U_FormHandler_Template_Error(t *testing.T) {
+	// pass invalid hex strings
+	req := httptest.NewRequest("POST", "/form", strings.NewReader("%zzzzz"))
 
-  name := req.Form.Get("name")
-  if name == "" {
-    name = "World"
-  }
-  fmt.Fprintf(res, "Posted Hello, %s!", name)
+	// set the header `Content-Type` to `application/x-www-form-urlencoded`
+  req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	// create a new httptest.NewRecorder
+	res := httptest.NewRecorder()
+
+	// Call the FormHandler
+  FormHandler(res, req)
+
+	// Test to see the the response code is 500
+  if got, exp := res.Code, http.StatusInternalServerError; got != exp {
+		t.Errorf("unexpected response code.  got: %d, exp %d\n", got, exp)
+	}
+
+	// test the body is `Oops!`
+  if got, exp := res.Body.String(), "Oops!"; got != exp {
+		t.Errorf("unexpected body.  got: %s, exp %s\n", got, exp)
+	}
 }
