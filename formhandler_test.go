fdbbfd8e9617a157b8a745d7c5d8dd348ae859f7
Nathan Marley <nathan.marley@gmail.com>
2018-08-27 13:33:40 -0600

    ?

--- formhandler_test.go
+++ formhandler_test.go
@@ -1,6 +1,7 @@
 package src_test
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
@@ -9,18 +10,20 @@ import (
 )
 
 func Test_U_FormHandler(t *testing.T) {
-	// The url.Values type allows us to assemble a "form" that we can send as part of the request.
+	// The url.Values type allows us to assemble a "form" that we can send as part
+	// of the request.
 	form := url.Values{}
 	form.Add("name", "Ringo")
 
-	// The `Encode` method on `url.Values` will properly encode the values we set into well formed `string` that can be read as the body of the request.
+	// The `Encode` method on `url.Values` will properly encode the values we set
+	// into well formed `string` that can be read as the body of the request.
 	req := httptest.NewRequest("POST", "/form", strings.NewReader(form.Encode()))
 
 	// We must set the `Content-Type` correctly for `ParseForm` to work.
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	res := httptest.NewRecorder()
 
-	// FormHandler(res, req)
+	FormHandler(res, req)
 
 	if got, exp := res.Code, http.StatusOK; got != exp {
 		t.Errorf("unexpected response code.  got: %d, exp %d\n", got, exp)
@@ -28,4 +31,18 @@ func Test_U_FormHandler(t *testing.T) {
 	if got, exp := res.Body.String(), "Posted Hello, Ringo!"; got != exp {
 		t.Errorf("unexpected body.  got: %s, exp %s\n", got, exp)
 	}
+}
+
+func FormHandler(res http.ResponseWriter, req *http.Request) {
+  err := req.ParseForm()
+  if err != nil {
+    res.WriteHeader(500)
+    return
+  }
+
+  name := req.Form.Get("name")
+  if name == "" {
+    name = "World"
+  }
+  fmt.Fprintf(res, "Posted Hello, %s!", name)
 }
